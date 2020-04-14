3cc232a005855e6d2c5bb98edaedb7d056a1bc1a
Mahendra AR <mahendra0859@gmail.com>
2020-04-15 04:45:02 +0530

    🐳 build files added to docs folder for deployment

---
+++ docs/precache-manifest.72015b61eca2bfa56637e22ef6383323.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10848e452ffda43f662fee657f0ad0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dceacd40730bb7b859d0",
+    "url": "/static/css/main.323eb518.chunk.css"
+  },
+  {
+    "revision": "471b1802b12a98b6f6dd",
+    "url": "/static/js/2.ae094073.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ae094073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dceacd40730bb7b859d0",
+    "url": "/static/js/main.869e8e88.chunk.js"
+  },
+  {
+    "revision": "cc29aceb6f13d7387149",
+    "url": "/static/js/runtime-main.0216efa3.js"
+  }
+]);
